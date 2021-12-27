0eb4e6a7eee9da36666e0a9c3badf4fce589dbca
Jonson5454 <asgsdsdsa@gmail.com>
2021-12-27 11:13:11 +0500

    Initial Commit

---
+++ sowhat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sowhat
+//
+//  Created by a on 12/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
